2855a86d400a3db78846fa12cba36d8387253464
ZhongQian TiaoGong <Qiangong2@users.noreply.github.com>
2018-01-03 14:03:56 -0800

    Update travis.sh
    fix automated build errors

--- travis.sh
+++ travis.sh
@@ -4,4 +4,5 @@ export ARCH=arm
 export KBUILD_BUILD_USER="Qiangong2"
 export KBUILD_BUILD_HOST="QG2ZHT1"
 make d5028m_blu_gmo_defconfig
+mkdir -p /home/travis/build/Qiangong2/android_kernel_blu_d790q/tools/tools
 make zImage
