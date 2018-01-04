2cf459485a8c7a17a09dd636fb5890826981bf96
ZhongQian TiaoGong <Qiangong2@users.noreply.github.com>
2018-01-04 15:08:58 -0800

    Update travis.sh
    whoops. need to change that...

--- travis.sh
+++ travis.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/Qiangong2/bluapprovedarmeabi4-8
-export CROSS_COMPILE=./arm-eabi-4.8/bin/arm-eabi- 
+export CROSS_COMPILE=./bluapprovedarmeabi4-8/arm-eabi-4.8/bin/arm-eabi- 
 export ARCH=arm
 export KBUILD_BUILD_USER="Qiangong2"
 export KBUILD_BUILD_HOST="QG2ZHT1"
